0646a2d54b5f4bfcf4cf51038b4a8528cc850be6
Christian Kaltenecker <kaltenec@devclipse.de>
2024-02-29 17:07:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is me!'));
+app.get('/', (req, res) => res.send('Welcome to my hello world script!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
